38db46d861aae0c60a1b50da8fde37ce2304898c
Bill Erickson <bill.erickson@gmail.com>
2017-04-09 20:32:41 -0500

    use field ids from settings page

--- class-be-wpforms-convertkit.php
+++ class-be-wpforms-convertkit.php
@@ -23,122 +23,102 @@ class BE_WPForms_ConvertKit {
     }
 
     /**
-     * ConvertKit Fields
+     * Add Settings Section
      *
      */
-    function convertkit_fields() {
-
-        $fields = array(
-            array(
-                'slug'      => 'first_name',
-                'label'     =>  __( 'First Name', 'be_wpforms_convertkit' ),
-                'field_map' => array( 'text', 'name' ),
-            ),
-            array(
-                'slug'      => 'email',
-                'label'     => __( 'Email Address', 'be_wpforms_convertkit' ),
-                'field_map' => array( 'email' ),
-            )
-        );
-
-        return apply_filters( 'be_wpforms_convertkit_fields', $fields );
+    function settings_section( $sections, $form_data ) {
+        $sections['be_convertkit'] = __( 'ConvertKit', 'be_wpforms_convertkit' );
+        return $sections;
     }
 
 
-     /**
-      * Add Settings Section
-      *
-      */
-     function settings_section( $sections, $form_data ) {
-         $sections['be_convertkit'] = __( 'ConvertKit', 'be_wpforms_convertkit' );
-         return $sections;
-     }
+    /**
+     * ConvertKit Settings Content
+     *
+     */
+    function settings_section_content( $instance ) {
+        echo '<div class="wpforms-panel-content-section wpforms-panel-content-section-be_convertkit">';
+        echo '<div class="wpforms-panel-content-section-title">' . __( 'ConvertKit', 'be_wpforms_convertkit' ) . '</div>';
 
+        wpforms_panel_field(
+            'text',
+            'settings',
+            'be_convertkit_api',
+            $instance->form_data,
+            __( 'ConvertKit API Key', 'be_wpforms_convertkit' )
+        );
 
-     /**
-      * ConvertKit Settings Content
-      *
-      */
-     function settings_section_content( $instance ) {
-         echo '<div class="wpforms-panel-content-section wpforms-panel-content-section-be_convertkit">';
-         echo '<div class="wpforms-panel-content-section-title">' . __( 'ConvertKit', 'be_wpforms_convertkit' ) . '</div>';
+        wpforms_panel_field(
+            'text',
+            'settings',
+            'be_convertkit_form_id',
+            $instance->form_data,
+            __( 'ConvertKit Form ID', 'be_wpforms_convertkit' )
+        );
 
-         wpforms_panel_field(
-             'text',
-             'settings',
-             'be_convertkit_api',
-             $instance->form_data,
-             __( 'ConvertKit API Key', 'be_wpforms_convertkit' )
-         );
+        wpforms_panel_field(
+            'select',
+            'settings',
+            'be_convertkit_field_first_name',
+            $instance->form_data,
+            __( 'First Name', 'be_wpforms_convertkit' ),
+            array(
+                'field_map'   => array( 'text', 'name' ),
+                'placeholder' => __( '-- Select Field --', 'be_wpforms_convertkit' ),
+            )
+        );
 
-         wpforms_panel_field(
-             'text',
-             'settings',
-             'be_convertkit_form_id',
-             $instance->form_data,
-             __( 'ConvertKit Form ID', 'be_wpforms_convertkit' )
-         );
+        wpforms_panel_field(
+            'select',
+            'settings',
+            'be_convertkit_field_email',
+            $instance->form_data,
+            __( 'Email Address', 'be_wpforms_convertkit' ),
+            array(
+                'field_map'   => array( 'email' ),
+                'placeholder' => __( '-- Select Field --', 'be_wpforms_convertkit' ),
+            )
+        );
 
-         $fields = $this->convertkit_fields();
-         foreach( $fields as $field ) {
+        echo '</div>';
+    }
 
-             wpforms_panel_field(
-                 'select',
-                 'settings',
-                 'be_convertkit_' . sanitize_html_class( $field['slug'] ),
-                 $instance->form_data,
-                 esc_html( $field['label'] ),
-                 array(
-                     'field_map'   => $field['field_map'],
-                     'placeholder' => __( '-- Select Field --', 'be_wpforms_convertkit' ),
-                 )
-             );
+    /**
+     * Integrate WPForms with ConvertKit
+     *
+     */
+    function send_data_to_convertkit( $fields, $entry, $form_data, $entry_id ) {
 
-         }
+        // Get API key and CK Form ID
+        $api_key = $ck_form_id = false;
+        if( !empty( $form_data['settings']['be_convertkit_api'] ) )
+            $api_key = esc_html( $form_data['settings']['be_convertkit_api'] );
+        if( !empty( $form_data['settings']['be_convertkit_form_id'] ) )
+            $ck_form_id = intval( $form_data['settings']['be_convertkit_form_id'] );
 
+        if( ! ( $api_key && $ck_form_id ) )
+            return;
 
-         echo '</div>';
-     }
+        // Get email and first name
+        $email_field_id = $form_data['settings']['be_convertkit_field_email'];
+        $first_name_field_id = $form_data['settings']['be_convertkit_field_first_name'];
 
-     /**
-      * Integrate WPForms with ConvertKit
-      *
-      */
-     function send_data_to_convertkit( $fields, $entry, $form_data, $entry_id ) {
+        if( ! ( $email_field_id && $first_name_field_id ) )
+            return;
 
-         // Get API key and CK Form ID
-         $api_key = $ck_form_id = false;
-         if( !empty( $form_data['settings']['be_convertkit_api'] ) )
-             $api_key = esc_html( $form_data['settings']['be_convertkit_api'] );
-         if( !empty( $form_data['settings']['be_convertkit_form_id'] ) )
-             $ck_form_id = intval( $form_data['settings']['be_convertkit_form_id'] );
+        $args = array(
+            'api_key'    => $api_key,
+            'email'      => $form_data['fields'][$email_field_id]['value'],
+            'first_name' => $form_data['fields'][$first_name_field_id]['value']
+        );
 
-         if( ! ( $api_key && $ck_form_id ) )
-             return;
+        if( empty( $args['email'] ) || empty( $args['first_name'] ) )
+            return;
 
-         // Get email and first name
-         $args = array( 'api_key' => $api_key, 'email' => false, 'first_name' => false );
-         foreach( $form_data['fields'] as $i => $field ) {
+        // Submit to ConvertKit
+        $request = wp_remote_post( add_query_arg( $args, 'https://api.convertkit.com/v3/forms/' . $ck_form_id . '/subscribe' ) );
 
-             $classes = !empty( $field['css'] ) ? explode( ' ', $field['css'] ) : array();
-
-             if( in_array( 'ck-first-name', $classes ) ) {
-                 $args['first_name'] = $fields[$i]['value'];
-             }
-
-             if( in_array( 'ck-email', $classes ) ) {
-                 $args['email'] = $fields[$i]['value'];
-             }
-
-         }
-
-         if( empty( $args['email'] ) || empty( $args['first_name'] ) )
-             return;
-
-         // Submit to ConvertKit
-         $request = wp_remote_post( add_query_arg( $args, 'https://api.convertkit.com/v3/forms/' . $ck_form_id . '/subscribe' ) );
-
-     }
+    }
 
 }
 new BE_WPForms_ConvertKit;
